496318781069826da5e5e16b24948ae735fe2004
LinnLattOo <linnlattoo@gusto.edu.mm>
2023-01-12 16:39:12 +0630

    Spring-Boots-tutorial

---
+++ Spring-Boots-tutorial/src/main/java/com/linnlatt/SpringBootstutorial/SpringBootsTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.linnlatt.SpringBootstutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootsTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootsTutorialApplication.class, args);
+	}
+
+}
---
+++ Spring-Boots-tutorial/src/test/java/com/linnlatt/SpringBootstutorial/SpringBootsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linnlatt.SpringBootstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootsTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
